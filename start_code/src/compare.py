8d3476e7614341d2d6dfce4c284fa1aaef39dffd
evelyn-sz <evelynszarek@gmail.com>
2020-11-25 22:14:40 +0000

    Committed all at once hehe

--- start_code/src/compare.py
+++ start_code/src/compare.py
@@ -1,2 +1,7 @@
-def compare():
-    pass
+def compare(number_1, number_2):
+    if num_1 < num_2:
+        return f"{num_1} is less than {num_2}"
+    elif num_1 > num_2:
+        return f"{num_1} is greater than {num_2}"
+    elif num_1 == num_2:    # rather than "else", in case a non-number is passed
+        return f"Both numbers are equal"
